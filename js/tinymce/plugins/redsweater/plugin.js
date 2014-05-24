cc5c996821439b823e7aae1cd5b2101a126548a1
Daniel Jalkut <jalkut@red-sweater.com>
2014-05-24 16:05:45 -0400

    Taking a stab at trying to add back a behavior where two returns in a row will exit a pre block, but it's giving me trouble getting all the details right - effectively having to try to mimic EnterKey.js functionality. I think I need to take another tack to get this right, hopefully without modifying EnterKey.js. Effectively what I want to achieve is the new default behavior of pressing return in PRE but with an easy way to still get out by repeated return key presses.

--- js/tinymce/plugins/redsweater/plugin.js
+++ js/tinymce/plugins/redsweater/plugin.js
@@ -47,4 +47,61 @@ tinymce.PluginManager.add('redsweater', function(editor) {
 		});
 	});
 
+	// In TinyMCE3 we suffered the terrible behavior of PRE blocks that return would add a new PRE block so that
+	// there was a long string of them. They've fixed that in TinyMCE4 however the behavior is now such that
+	// carriage returns map to <br /> by default, making it impossible to get out intuititively. Until we support
+	// a more obvious way to get out of a pre block (e.g. by clicking some subtle UI that appears while editing one),
+	// we want to maintain the old behavior at least that if you press return twice at the very end of a PRE block
+	// it will get you out.
+	function nodeInserted(changeEvent) {
+		// Only interested if we inserted a BR into a PRE
+		if ((changeEvent.relatedNode.nodeName === 'PRE') && (changeEvent.target.nodeName === 'BR'))
+		{
+			// Did we just insert a second BR in a row at the end of the PRE block?
+			// Note that it's weird because sometimes the new br will be inserted BEFORE the existing
+			// br at the end, and sometimes after. So two in a row in either case means times to remove
+			// them both and add a new paragraph.
+			var parent = changeEvent.relatedNode;
+			var brNode = changeEvent.target;
+			var brPrevious = changeEvent.target.previousElementSibling;
+			var brNext = changeEvent.target.nextElementSibling;
+
+			// TinyMCE does some shenanigans where they actually put an extra BR in at the end of the block,
+			// for some reason (see "extraBr" in EnterKey.js), so we accept as two BR's "at the end" an
+			// inserted BR with a BR previous sibling and a BR next sibling whose next sibling is null.
+			if (brPrevious && (brPrevious.nodeName === 'BR'))
+			{
+				if (brNext && (brNext.nodeName === 'BR') && (brNext.nextElementSibling == null))
+				{
+					// Zap the two BR nodes that led to this condition, but do so after a delay so
+					// the TinyMCE code that is currently inserting these breaks doesn't hit an exception
+					// trying to work with the added BR
+					window.setTimeout(function (par, first, second) {
+						par.removeChild(first);
+						par.removeChild(second);
+
+						// Create a new P node (maybe should use forced_root_block from settings, but
+						// we always use P for that ourselves)
+						var newParagraph = editor.getDoc().createElement("P");
+						newParagraph.innerHTML = '<br data-mce-bogus="1">';
+						editor.dom.insertAfter(newParagraph, parent);
+						var newSelection = editor.selection.getRng();
+						newSelection.selectNode(newParagraph);
+						newSelection.collapse();
+						editor.selection.setRng(newSelection);
+					}, 100, parent, brPrevious, brNode);
+				}
+			}
+		}
+	}
+
+	// After the editor is done loading, pay attention to node insertions
+	editor.on("init", function (e) {
+		editor.getBody().addEventListener("DOMNodeInserted", nodeInserted, false);
+	});
+
+//	editor.on("change", function(nodeChangeEvent) {
+//		var theElement = nodeChangeEvent.element;
+//		console.log("Got node change on " + theElement.nodeName);
+//	});
 });
